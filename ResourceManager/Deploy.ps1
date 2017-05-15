6bf11f7ae10f9f85d7ceb622083d79a4c0ed220a
Jamalapuram <sairama.Jamalapuram@Fiserv.com>
2017-05-15 10:31:02 -0700

    resource group manager

---
+++ ResourceManager/Deploy.ps1
@@ -0,0 +1,7 @@
+﻿Set-AzureRmContext -SubscriptionID '821d6eee-fe66-4c21-8e8b-ab80122b0d35'
+#New-AzureRmResourceGroup -Name ExampleGroup -Location "West US"
+# New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleGroup -TemplateFile .\CreateStorageAccountSample.json
+# with properties specified.
+#New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleGroup -TemplateFile .\CreateStorageAccountSample.json -storageNamePrefix "testing"
+# removing
+Remove-AzureRmResourceGroup -Name ExampleGroup
