f57747b54a20961abede0c34448f3d12eb83dff6
Jamalapuram <sairama.Jamalapuram@Fiserv.com>
2018-10-05 13:48:10 -0700

    adding scripts

---
+++ powershell/Start-MyAzureEnvironment.ps1
@@ -0,0 +1,20 @@
+﻿
+Import-Module .\AzureCommon.psm1
+Login
+
+Select-AzureRmSubscription -Subscription <subidhere>
+$OldResourceGroupName = 'Default-ActivityLogAlerts'
+$ResourceGroupName = 'dev'
+$VMName = 'dev'
+$LoadbalancerName = 'dev-lb'
+
+if( $false -eq (Test-VMRunning -Name $VMName -ResourceGroupName $OldResourceGroupName) ){
+    Write-Host "Starting Virtual machine:$VMName"
+    Start-AzureRmVM -Name $VMName -ResourceGroupName $OldResourceGroupName
+}else{
+    Write-Host "VM: $VMName is already running."
+}
+
+Write-Host "Getting public ipaddress to be connected"
+Get-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroupName | select IPAddress
+
