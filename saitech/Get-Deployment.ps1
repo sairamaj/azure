cb47593b70e813fa693735a92678e22533a9d7fe
Jamalapuram <sairama.Jamalapuram@Fiserv.com>
2019-04-08 16:29:50 -0700

    deployment

---
+++ saitech/Get-Deployment.ps1
@@ -0,0 +1,6 @@
+﻿for($i = 0 ; $i -lt 10; $i++)
+{
+    $status = Get-AzureRmResourceGroupDeployment -Name TestAutomationDeployment3 -ResourceGroupName deleteme
+    $status.ProvisioningState
+    Start-Sleep -Seconds 30
+}
---
+++ saitech/New-Deployment.ps1
@@ -0,0 +1,19 @@
+﻿$ResourceGroupName = 'deleteme'
+$TemplateFile = 'Templates\api-deploy.json'
+$TemplateParameterFile = 'Templates\api-deploy-parameters.json'
+
+# Creating a deployment 
+
+
+# Validte the template.
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+    -TemplateParameterFile $TemplateParameterFile -TemplateFile $TemplateFile
+
+# Deploy
+
+New-AzureRmResourceGroupDeployment `
+    -Name TestAutomationDeployment3 `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile $TemplateFile `
+    -TemplateParameterFile $TemplateParameterFile `
+    -AsJob
